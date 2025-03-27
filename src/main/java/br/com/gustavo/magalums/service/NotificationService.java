63a6124f4217c6275dfeb3094c727a0a1a1801e6
Guh-paixao <gustavopaixao.dev@gmail.com>
2025-03-27 00:30:38 -0300

    feat: implement notification checking and sending in NotificationService and MagaluTaskScheduler

--- src/main/java/br/com/gustavo/magalums/service/NotificationService.java
+++ src/main/java/br/com/gustavo/magalums/service/NotificationService.java
@@ -6,7 +6,10 @@ import br.com.gustavo.magalums.entity.Status;
 import br.com.gustavo.magalums.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class NotificationService {
@@ -32,6 +35,23 @@ public class NotificationService {
             notification.get().setStatus(Status.Values.CANCELED.toStatus());
             notificationRepository.save(notification.get());
         }
+    }
 
+    public void checkAndSend(LocalDateTime dateTime) {
+        var notifications = notificationRepository.findByStatusInAndDateTimeBefore(
+                List.of(Status.Values.PENDING.toStatus(), Status.Values.ERROR.toStatus()),
+                dateTime
+        );
+
+        notifications.forEach(sendNotification());
+    }
+
+    private Consumer<Notification> sendNotification() {
+        return n -> {
+            // TODO - realizar o envio da notificação
+
+            n.setStatus(Status.Values.SUCCESS.toStatus());
+            notificationRepository.save(n);
+        };
     }
 }
